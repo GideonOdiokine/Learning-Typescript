6d76c92b39dade2d9525bfa62ba46bf7865e4c5e
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-25 21:09:47 +0100

    Finished learning the basic

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// BASIC
+let id = 5;
+let company = "Gideon Odioks";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, "Gideon", true];
+// Tuple
+let person = [1, "Gideon", true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'John'],
+    [2, 'Gideon'],
+    [3, 'Odiokine']
+];
+console.log(employee);
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+// BASIC
+let id: number=5
+let company: string ="Gideon Odioks"
+let isPublished: boolean=true
+
+let x: any="Hello"
+let ids: number[] =[1,2,3,4,5]
+
+let arr: any[] =[1,"Gideon", true]
+
+// Tuple
+let person: [number, string, boolean]= [1, "Gideon", true]
+
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+    [1, 'John'],
+    [2, 'Gideon'],
+    [3, 'Odiokine'],
+]
+console.log(employee)
