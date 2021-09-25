63d4e9b136346f89c4863d661b2725981b79682e
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-25 21:19:48 +0100

    Finished learning Enum

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,24 @@ let employee;
 employee = [
     [1, 'John'],
     [2, 'Gideon'],
-    [3, 'Odiokine']
+    [3, 'Odiokine'],
 ];
-console.log(employee);
+// Union
+let pid;
+pid = "22";
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Right"] = 3] = "Right";
+    Direction1[Direction1["Left"] = 4] = "Left";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up ";
+    Direction2["Down"] = "Down";
+    Direction2["Right"] = "Right";
+    Direction2["Left"] = "Left";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Left);
